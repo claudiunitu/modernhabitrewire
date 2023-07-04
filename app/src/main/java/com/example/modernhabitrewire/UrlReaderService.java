422b0fab138da79b23ce5b213e1149862ae057b9
claudiu.nitu <claudiu.nitu@hotmail.com>
2023-07-04 12:02:36 +0300

    attempt to fix service crashing

--- app/src/main/java/com/example/modernhabitrewire/UrlReaderService.java
+++ app/src/main/java/com/example/modernhabitrewire/UrlReaderService.java
@@ -47,57 +47,72 @@ public class UrlReaderService extends AccessibilityService {
             return;
         }
         final int eventType = accessibilityEvent.getEventType();
-        switch(eventType) {
-            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED: {
+        if(eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) {
 
-                AccessibilityNodeInfo parentNodeInfo = accessibilityEvent.getSource();
-                if (parentNodeInfo == null) {
-                    return;
-                }
-
-                String packageName = accessibilityEvent.getPackageName().toString();
-
-                // check if it is a forbidden package
-                if(isForbiddenPackage(packageName)) {
-//                    Log.d("UrlReaderService", packageName + "  :  " + "denied");
-                    this.performRedirect("localhost", packageName);
-                    return;
-                }
-
-
-                // check if the event was triggered by a browser and if it accessed a forbidden url
-                SupportedBrowserConfig browserConfig = null;
-                for (SupportedBrowserConfig supportedConfig: supportedBrowsers) {
-                    if (supportedConfig.packageName.equals(packageName)) {
-                        browserConfig = supportedConfig;
-                    }
-                }
-                //this is not supported browser, so exit
-                if (browserConfig == null) {
-//                    Log.d("UrlReaderService", "Not a browser.");
-                    return;
-                }
-
-                String capturedUrl = captureUrl(parentNodeInfo, browserConfig);
-                parentNodeInfo.recycle();
-
-                if (capturedUrl == null) {
-                    return;
-                }
-
-                if(android.util.Patterns.WEB_URL.matcher(capturedUrl).matches() && this.isForbiddenWebsite(capturedUrl)) {
-//                    Log.d("UrlReaderService", packageName + "  :  " + capturedUrl);
-                    this.performRedirect("localhost", packageName);
-                    return;
-                }
-
-
-
-
+            AccessibilityNodeInfo parentNodeInfo = accessibilityEvent.getSource();
+            if (parentNodeInfo == null) {
+                return;
             }
-            break;
+
+            String packageName = accessibilityEvent.getPackageName().toString();
+
+            this.redirectIfNeeded( parentNodeInfo, packageName);
+
+            parentNodeInfo.recycle();
+
         }
     }
+
+    private void redirectIfNeeded(AccessibilityNodeInfo parentNodeInfo,String packageName) {
+        // check if it is a forbidden package
+        if(this.redirectIfForbiddenPackage(packageName)) {
+            return;
+        }
+
+        // check if forbidden url
+        if(this.redirectIfForbiddenUrl (parentNodeInfo,packageName) ){
+            return;
+        }
+    }
+
+    private Boolean redirectIfForbiddenPackage(String packageName) {
+        if(isForbiddenPackage(packageName)) {
+            Log.d("UrlReaderService", packageName + "  :  " + "denied");
+            this.performRedirect();
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean redirectIfForbiddenUrl (AccessibilityNodeInfo parentNodeInfo,String packageName){
+        // check if the event was triggered by a browser and if it accessed a forbidden url
+        SupportedBrowserConfig browserConfig = null;
+        for (SupportedBrowserConfig supportedConfig: supportedBrowsers) {
+            if (supportedConfig.packageName.equals(packageName)) {
+                browserConfig = supportedConfig;
+            }
+        }
+        //this is not supported browser, so exit
+        if (browserConfig == null) {
+            Log.d("UrlReaderService", "Not a browser.");
+            return false;
+        }
+
+        String capturedUrl = captureUrl(parentNodeInfo, browserConfig);
+
+
+        if (capturedUrl == null) {
+            return false;
+        }
+
+        if(android.util.Patterns.WEB_URL.matcher(capturedUrl).matches() && this.isForbiddenWebsite(capturedUrl)) {
+            Log.d("UrlReaderService", packageName + "  :  " + capturedUrl);
+            this.performRedirect();
+            return true;
+        }
+        return false;
+    }
+
 
     private static class SupportedBrowserConfig {
         public String packageName, addressBarId;
@@ -121,22 +136,7 @@ public class UrlReaderService extends AccessibilityService {
 
         return browsers;
     }
-    private void getChild(AccessibilityNodeInfo info)
-    {
-        int i=info.getChildCount();
-        for(int p=0;p<i;p++)
-        {
-            AccessibilityNodeInfo n=info.getChild(p);
-            if(n!=null) {
-                String strres = n.getViewIdResourceName();
-                if (n.getText() != null) {
-                    String txt = n.getText().toString();
-//                    Log.d("Track", strres + "  :  " + txt);
-                }
-                getChild(n);
-            }
-        }
-    }
+
     private String captureUrl(AccessibilityNodeInfo info, SupportedBrowserConfig config) {
 
         List<AccessibilityNodeInfo> nodes = info.findAccessibilityNodeInfosByViewId(config.addressBarId);
@@ -155,10 +155,8 @@ public class UrlReaderService extends AccessibilityService {
         return url;
     }
 
-    private void performRedirect(String redirectUrl, String browserPackage) {
-
+    private void performRedirect() {
         performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
-
     }
 
     private boolean isForbiddenWebsite(String url) {
@@ -182,13 +180,13 @@ public class UrlReaderService extends AccessibilityService {
 
     @Override
     public void onInterrupt() {
-//        Log.d("UrlReaderService", "Error.");
+        Log.e("UrlReaderService", "Error.");
     }
 
     @Override
     public void onServiceConnected() {
 
-//        Log.d("UrlReaderService", "Connected.");
+        Log.d("UrlReaderService", "Connected.");
         this.registerChargerBroadcastReceiver();
 
     }
